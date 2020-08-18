90aa8a1ba3a414b5bd51e2ef553bfd4cb30e324a
rosaneprado <rosane.prado@gmail.com>
2020-08-18 10:54:55 -0400

    Update server.py

--- auth0-pocRP/server.py
+++ auth0-pocRP/server.py
@@ -26,7 +26,6 @@ AUTH0_CLIENT_ID = env.get(constants.AUTH0_CLIENT_ID)
 AUTH0_CLIENT_SECRET = env.get(constants.AUTH0_CLIENT_SECRET)
 AUTH0_DOMAIN = env.get(constants.AUTH0_DOMAIN)
 AUTH0_BASE_URL = 'https://dev-psgi167i.us.auth0.com'
-"""AUTH0_AUDIENCE = env.get(constants.AUTH0_AUDIENCE)
 
 app = Flask(__name__, static_url_path='/public', static_folder='./public')
 app.secret_key = constants.SECRET_KEY
