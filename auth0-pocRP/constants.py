3f9184f816a9c4a03014a97d9a6101c5dbb8dfb7
rosaneprado <rosane.prado@gmail.com>
2020-08-18 09:03:28 -0400

    Update constants.py

--- auth0-pocRP/constants.py
+++ auth0-pocRP/constants.py
@@ -1,5 +1,2 @@
 AUTH0_CLIENT_ID = 'aBGU78bNDSbSe4eGzZXqN9isMHuf4jl8'
-AUTH0_CLIENT_SECRET = 'zrLy6cN_YN2RwM9JUMqChIqTcfJZlignALBXTROg8XMptngF2rIwrSRvwLwQDoIz'
-AUTH0_CALLBACK_URL = 'https://pocpy.herokuapp.com/callback'
 AUTH0_DOMAIN = 'dev-psgi167i.us.auth0.com'
-JWT_PAYLOAD = 'jwt_payload'
