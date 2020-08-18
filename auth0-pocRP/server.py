30166c814979e63bf7e0516c1bd9f3c86f7d9d3c
rosaneprado <rosane.prado@gmail.com>
2020-08-18 10:50:08 -0400

    Update server.py

--- auth0-pocRP/server.py
+++ auth0-pocRP/server.py
@@ -25,7 +25,7 @@ AUTH0_CALLBACK_URL = env.get(constants.AUTH0_CALLBACK_URL)
 AUTH0_CLIENT_ID = env.get(constants.AUTH0_CLIENT_ID)
 AUTH0_CLIENT_SECRET = env.get(constants.AUTH0_CLIENT_SECRET)
 AUTH0_DOMAIN = env.get(constants.AUTH0_DOMAIN)
-AUTH0_BASE_URL = 'https://' + AUTH0_DOMAIN
+AUTH0_BASE_URL = 'https://' + env.get(constants.AUTH0_DOMAIN)
 AUTH0_AUDIENCE = env.get(constants.AUTH0_AUDIENCE)
 
 app = Flask(__name__, static_url_path='/public', static_folder='./public')
