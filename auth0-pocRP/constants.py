4956ca742966b500a6565b36ee485721edaebd76
rosaneprado <rosane.prado@gmail.com>
2020-08-18 11:00:44 -0400

    Update constants.py

--- auth0-pocRP/constants.py
+++ auth0-pocRP/constants.py
@@ -2,4 +2,8 @@ AUTH0_CLIENT_ID = 'aBGU78bNDSbSe4eGzZXqN9isMHuf4jl8'
 AUTH0_CLIENT_SECRET = 'zrLy6cN_YN2RwM9JUMqChIqTcfJZlignALBXTROg8XMptngF2rIwrSRvwLwQDoIz'
 AUTH0_CALLBACK_URL = 'https://pocpy.herokuapp.com/callback'
 AUTH0_DOMAIN = 'dev-psgi167i.us.auth0.com'
+AUTH0_AUDIENCE = 'AUTH0_AUDIENCE'
+PROFILE_KEY = 'profile'
+SECRET_KEY = 'ThisIsTheSecretKey'
 JWT_PAYLOAD = 'jwt_payload'
+
