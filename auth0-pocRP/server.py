b4b12aa13a2c17c40a8260262093ffb880705c8b
rosaneprado <rosane.prado@gmail.com>
2020-08-21 23:53:20 -0400

    Update server.py

--- auth0-pocRP/server.py
+++ auth0-pocRP/server.py
@@ -89,7 +89,8 @@ def calc(value1, value2):
 
     sv = AsymmetricSignatureVerifier(jwks_url)  # Reusable instance
     tv = TokenVerifier(signature_verifier=sv, issuer=issuer, audience='https://pocpy.herokuapp.com/calc')
-    tv.verify(id_token)
+    #tv.verify(id_token)
+    verifier(id_token, jwks_url)
 
     result = int(value1) + int(value2)
     return str(result)
