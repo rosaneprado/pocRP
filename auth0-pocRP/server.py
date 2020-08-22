dc1fcbfa6b93bbc6e3204569827879375cf51dac
rosaneprado <rosane.prado@gmail.com>
2020-08-22 00:40:10 -0400

    Update server.py

--- auth0-pocRP/server.py
+++ auth0-pocRP/server.py
@@ -113,20 +113,19 @@ def home():
 
 @app.route('/calc/<value1>/<value2>')
 def calc(value1, value2):
-    print("teste")
-    id_token = request.headers["Authorization"][7:]
+    return ("teste")
+    #id_token = request.headers["Authorization"][7:]
 
-    jwks_url = 'https://{}/calc/.well-known/jwks.json'.format(constants.AUTH0_DOMAIN)
-    issuer = 'https://{}/'.format(constants.AUTH0_DOMAIN)
+    #jwks_url = 'https://{}/calc/.well-known/jwks.json'.format(constants.AUTH0_DOMAIN)
+    #issuer = 'https://{}/'.format(constants.AUTH0_DOMAIN)
 
-    sv = AsymmetricSignatureVerifier(jwks_url)  # Reusable instance
-    tv = TokenVerifier(signature_verifier=sv, issuer=issuer, audience='https://pocpy.herokuapp.com/calc')
+    #sv = AsymmetricSignatureVerifier(jwks_url)  # Reusable instance
+    #tv = TokenVerifier(signature_verifier=sv, issuer=issuer, audience='https://pocpy.herokuapp.com/calc')
     #tv.verify(id_token)
-    verify(id_token, jwks_url)
+    #verify(id_token, jwks_url)
 
-    result = int(value1) + int(value2)
-    import json
-    print (json.dumps(result))
+    #result = int(value1) + int(value2)
+    
     return str(result)
     
 
